99bd85681567ba789beb97746d34d6504413c987
nizarfuad <nizarfuad123@gmail.com>
2022-02-24 20:25:29 +0700

    New Model ALERT

--- js/global.js
+++ js/global.js
@@ -2,21 +2,19 @@ function Announcer() {
   const Item = document.querySelector("#topAnnouncer");
   let array = 0;
 
-  const Instagram = `<a class="u-00 text-danger" href="https://www.instagram.com/nizar.gdem/" target="_blank"><b>nizar.gdem</b></a>`;
-  const Nazaar = `<a class="u-00 text-danger" href="https://www.instagram.com/nazaar.store/" target="_blank"><b>Nazaar.Store</b></a>`;
-  const Whatsapp = `<a class="u-00 text-danger" href="https://api.whatsapp.com/send?phone=6282313542756" target="_blank"><b>Whatsapp</b></a>`;
-  const Tiktok = `<a class="u-00 text-danger" href="https://tiktok.com/@nazaarnizar" target="_blank"><b>@nazaarnizar</b></a>`;
+  const Instagram = `<a class="u-00 text-danger" href="https://www.instagram.com/nizar.gdem/" target="_blank">nizar.gdem</a>`;
+  const Nazaar = `<a class="u-00 text-danger" href="https://www.instagram.com/nazaar.store/" target="_blank">Nazaar.Store</a>`;
+  const Tiktok = `<a class="u-00 text-danger" href="https://tiktok.com/@nazaarnizar" target="_blank">@nazaarnizar</a>`;
 
-  const f1 = `<p class="animation mb-0 text-truncate">TOPUP DI INSTAGRAM ${Nazaar} YGY</p>`;
+  const f1 = `<p class="animation mb-0 text-truncate">Topup diamond murah di instagram ${Nazaar}</p>`;
   const f2 = `<p class="animation mb-0 text-truncate">Follow instagram pribadi gua ${Instagram}</p>`;
   const f3 = `<p class="animation mb-0 text-truncate">Nazaar Store, layanan topup trusted</p>`;
   const f4 = `<p class="animation mb-0 text-truncate">Stay Safe guys</p>`;
-  const f5 = `<p class="animation mb-0 text-truncate">Follow tiktok pribadi gua guys ${Tiktok}</p>`;
-  const f6 = `<p class="animation mb-0 text-truncate">Kalo mau mabar chat ${Whatsapp} saya guys</p>`;
-  const f7 = `<p class="animation mb-0 text-truncate">Thanks for visiting my website <3</p>`;
+  const f5 = `<p class="animation mb-0 text-truncate">Temukan konten menarik di tiktok pribadi gua: ${Tiktok}</p>`;
+  const f6 = `<p class="animation mb-0 text-truncate">Thanks for visiting my website <3</p>`;
 
-  let arrayEl = [f1, f2, f3, f4, f5, f6, f7];
-  let arrayMax = 7;
+  let arrayEl = [f1, f2, f3, f4, f5, f6];
+  let arrayMax = 6;
   setInterval(() => {
     Item.innerHTML = arrayEl[array];
 
--- js/system.js
+++ js/system.js
@@ -5,6 +5,25 @@ const inWinrate = document.getElementById("winrate");
 const inMatch = document.getElementById("match");
 const inTarget = document.getElementById("target");
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: "top-end",
+  showConfirmButton: false,
+  timer: 3000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener("mouseenter", Swal.stopTimer);
+    toast.addEventListener("mouseleave", Swal.resumeTimer);
+  },
+});
+
+const Confirms = Swal.mixin({
+  customClass: {
+    confirmButton: "btn btn-danger",
+  },
+  buttonsStyling: false,
+});
+
 Button.addEventListener("click", function () {
   const outWinrate = parseFloat(inWinrate.value);
   const outMatch = parseFloat(inMatch.value);
@@ -25,20 +44,61 @@ Button.addEventListener("click", function () {
 
 function validation(outMatch, outWinrate, outTarget, Final) {
   if (!outWinrate || !outMatch || !outTarget) {
-    Results.innerHTML = "Field harus diisi.";
-  } else if (outWinrate == 100 || outTarget == 100) {
-    Results.innerHTML = "Kamu perlu <b>0</b> win tanpa lose untuk mencapai Winrate <b>" + outTarget + "%</b>";
+    Toast.fire({
+      icon: "error",
+      title: "Field tidak boleh kosong",
+    });
+  } else if (outTarget == 100 && outWinrate == 100) {
+    Confirms.fire({
+      title: "Kamu membutuhkan",
+      html: "<strong>" + "0" + "</strong>" + " match untuk meraih Winrate " + "<strong>" + outTarget + "%</strong>",
+      icon: "info",
+      confirmButtonText: "Mantap!",
+      reverseButtons: true,
+    });
+  } else if (outMatch == 0 && outWinrate == 0 && outTarget == 100) {
+    Confirms.fire({
+      title: "Kamu membutuhkan",
+      html: "<strong>" + "1" + "</strong>" + " win tanpa lose untuk meraih Winrate " + "<strong>" + outTarget + "%</strong>",
+      icon: "info",
+      confirmButtonText: "Mantap!",
+      reverseButtons: true,
+    });
+  } else if (outWinrate < 100 && outTarget == 100) {
+    Toast.fire({
+      icon: "error",
+      title: "Gabisa bro, jangan aneh aneh",
+    });
   } else if (outWinrate < 0 || outMatch < 0 || outTarget < 0) {
-    Results.innerHTML = "Field tidak boleh lebih kecil dari <b>0</b>";
+    Toast.fire({
+      icon: "error",
+      title: "Field tidak boleh kurang dari Nol",
+    });
   } else if (outTarget > 100 || outWinrate > 100) {
-    Results.innerHTML = "Winrate tidak boleh lebih dari <b>100%</b>";
-  } else if (outWinrate > outTarget) {
-    Results.innerHTML = "Kamu perlu <b> " + Final + " </b> lose tanpa win untuk mencapai Winrate <b>" + outTarget + "%</b>.";
-  } else if (outTarget == 100) {
-    Results.innerHTML = "Ga akan bisa bro, awkoakwoakw.";
+    Toast.fire({
+      icon: "warning",
+      title: "Winrate harus kurang atau sama dengan 100%",
+    });
   } else if (Final >= 100000) {
-    Results.innerHTML = "Jangan banyak ngarep bro, lu butuh 100k++ match win tanpa lose.";
+    Toast.fire({
+      icon: "warning",
+      title: "Yakin lu bro? lu butuh 100.000++ match!!!",
+    });
+  } else if (Final < 1) {
+    Confirms.fire({
+      title: "Kamu membutuhkan",
+      html: "<strong>" + Number(Final.toString().replace(/[-]/g, "")) + "</strong>" + " lose tanpa win untuk meraih Winrate " + "<strong>" + outTarget + "%</strong>",
+      icon: "info",
+      confirmButtonText: "Mantap!",
+      reverseButtons: true,
+    });
   } else {
-    Results.innerHTML = "Kamu perlu <b> " + Final + " </b> win tanpa lose untuk mencapai Winrate <b>" + outTarget + "%</b>.";
+    Confirms.fire({
+      title: "Kamu membutuhkan",
+      html: "<strong>" + Number(Final.toString().replace(/[-]/g, "")) + "</strong>" + " win tanpa lose untuk meraih Winrate " + "<strong>" + outTarget + "%</strong>",
+      icon: "info",
+      confirmButtonText: "Mantap!",
+      reverseButtons: true,
+    });
   }
 }
