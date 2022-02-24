f7e5c4d401722114640a9cc9bc19f15f113b47c8
nizarfuad <nizarfuad123@gmail.com>
2022-02-24 20:36:33 +0700

    Middle top alert edit

--- js/system.js
+++ js/system.js
@@ -7,7 +7,7 @@ const inTarget = document.getElementById("target");
 
 const Toast = Swal.mixin({
   toast: true,
-  position: "top-end",
+  position: "top",
   showConfirmButton: false,
   timer: 3000,
   timerProgressBar: true,
